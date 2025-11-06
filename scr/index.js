835924e39fc0efd770cb7fe305ce025314a1da4b
Maria Clara Silva <maria.silvamelo@eaportal.org>
2025-11-06 18:42:27 -0300

    eudb

--- scr/index.js
+++ scr/index.js
@@ -6,10 +6,24 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(people)
     
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        E-mail: ${email},
+        Idade: ${age},
+        Senha: ${password}
+        `)
+
+        response.status(201).json({ message: "Usúario cadastrado com sucesso!"})
 })
 
 app.listen(port, () => {
