74dd4d42c56fcb177b247eedf99eb86f60abefb8
Maria Clara Silva <maria.silvamelo@eaportal.org>
2025-11-24 20:38:00 -0300

    bruh

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,8 @@
 import express from "express"
-import { people } from "./people.js"
 import cors from "cors"
+import mysql from "mysql2"
+
+const { DATABASE_HOST, DATABASE_NAME,DATABASE_USER, DATABASE_PASSWORD } = process.env
 
 const app = express()
 const port = 3333
@@ -9,23 +11,74 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (request, response) => {
-    response.json(people)
+    const selecCommand = "SELECT name, email, age FROM mariaclara_02mc"
+    database.query(selecCommand, (error, users) => {
+        if (error) { 
+            console.log(error)
+            return
+
+        }
+
+        response.json(users)
+    })
+
     
 })
 
 app.post("/cadastrar", (request, response) => {
     const { name, email, age, password } = request.body.user
 
-    console.log(`
-        Nome: ${name},
-        E-mail: ${email},
-        Idade: ${age},
-        Senha: ${password}
-        `)
+    const inserCommand = `
+    INSERT INTO mariaclara_02mc(name, email, age, password)
+    VALUES (?, ?, ?, ?)
+    
+    `
 
-        response.status(201).json({ message: "Usúario cadastrado com sucesso!"})
+
+    database.query(inserCommand, [name, email, age, password], (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+           response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+
+    })
+
+
+
+})
+
+app.post("/login", (request, response) => {
+     const { email, password } = request.body.user
+
+     const selecCommand = "SELECT * FROM mariaclara_02mc WHERE email = ?"
+
+     database.query(selecCommand, [email], (error, user) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        if (user.length === 0 || password !== user[0].password) {
+            response.json ({message: "Email ou senha incorretos!"})
+           return
+         }
+
+        response.json({ id: user[0].id, name: user[0].name})
+     })
 })
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
+})
+
+
+ const database = mysql.createPool({
+    database: DATABASE_NAME,
+    host: DATABASE_HOST,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    connectionLimit: 10
+
 })
